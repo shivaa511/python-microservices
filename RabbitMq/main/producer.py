18793d770fa2fa88994151b311ce9558b5b15ef6
Siva Bathala <39061133+shivaa511@users.noreply.github.com>
2021-03-18 22:30:42 +0100

    first commin
    commit

---
+++ RabbitMq/main/producer.py
@@ -0,0 +1,12 @@
+import pika, json
+
+params = pika.URLParameters('amqps://vyoqndfg:Ntm9vJDjbkpSUmzYHaLuJRuuXMuW6Zjj@cow.rmq2.cloudamqp.com/vyoqndfg')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+
+def publish(method, body):
+    properties = pika.BasicProperties(method)
+    channel.basic_publish(exchange='', routing_key='admin', body=json.dumps(body), properties=properties)
